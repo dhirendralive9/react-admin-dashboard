6450fec000ae4712ee6a7cb31987db7b82270c46
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-07-01 23:20:02 +0530

    added RRD and routes.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List"
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    <Home />
+       <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="login" element={<Login/>} />
+      <Route path="users">
+        <Route index element={<List/>} />
+        <Route path=":userId" element={<Single/>}/>
+        <Route path="new" element={<New/>}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>
     </div>
   );
 }

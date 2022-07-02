6bb5049551db62e16af2c1f4283b7d81f0787288
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-07-02 16:38:17 +0530

    added and srarted with navbar.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,43 @@
 import './navbar.scss';
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import ListIcon from '@mui/icons-material/List';
 
 function Navbar() {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder='Search...'/>
+          <SearchIcon/>
+        </div>
+        <div className="items">
+          <div className="item">
+          <LanguageIcon />
+          </div>
+          <div className="item">
+          <DarkModeIcon />
+          </div>
+          <div className="item">
+          <CloseFullscreenIcon />
+          </div>
+          <div className="item">
+          <NotificationsIcon />
+          </div>
+          <div className="item">
+          <ChatBubbleIcon />
+          </div>
+          <div className="item">
+          <ListIcon />
+          </div>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 

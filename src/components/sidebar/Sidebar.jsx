052bee6912aa20bac2f7897a4ac9ff2a2271ebe4
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-07-02 13:22:19 +0530

    completed the Sidebar,added material UI.

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,30 +1,81 @@
 import './sidebar.scss';
+import GridViewIcon from '@mui/icons-material/GridView';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import GroupIcon from '@mui/icons-material/Group';
+import Inventory2Icon from '@mui/icons-material/Inventory2';
+import LocalPostOfficeIcon from '@mui/icons-material/LocalPostOffice';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
+import DnsIcon from '@mui/icons-material/Dns';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export const Sidebar = () => {
   return (
     <div className='sidebar'>
         <div className="top">
             <span className="logo">
-                OpenAdmin
+                OpAdmin
             </span>
         </div>
+        <hr />
         <div className="center">
             <ul>
+                <p className="title">Main</p>
                 <li>
+                    <GridViewIcon className='icon'/>
                     <span>Dashboard</span>
                 </li>
                 <li>
-                    <span>Dashboard</span>
+                    <AccountBoxIcon className='icon'/>
+                    <span>Profile</span>
                 </li>
                 <li>
-                    <span>Dashboard</span>
+                    <GroupIcon className='icon'/>
+                    <span>Users</span>
                 </li>
                 <li>
-                    <span>Dashboard</span>
+                    <Inventory2Icon className='icon'/>
+                    <span>Products</span>
+                </li>
+                <li>
+                    <LocalPostOfficeIcon className='icon'/>
+                    <span>Orders</span>
+                </li>
+                <p className="title">Utilities</p>
+                <li>
+                    <QueryStatsIcon className='icon'/>
+                    <span>Stats</span>
+                </li>
+                <li>
+                   <CircleNotificationsIcon className='icon'/>
+                    <span>Notifications</span>
+                </li>
+                <p className="title">Controls</p>
+                <li>
+                    <DnsIcon className='icon'/>
+                    <span>System</span>
+                </li>
+                <li>
+                    <SettingsIcon className='icon'/>
+                    <span>Settings</span>
+                </li>
+                <li>
+                    <ReceiptLongIcon className='icon'/>
+                    <span>Logs</span>
+                </li>
+                <li>
+                    <LogoutIcon className='icon'/>
+                    <span>Logout</span>
                 </li>
             </ul>
         </div>
-        <div className="bottom">Color Options</div>
+        <div className="bottom">
+            <div className="colorOption"></div>
+            <div className="colorOption"></div>
+            <div className="colorOption"></div>
+        </div>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div className='home'>
     <Sidebar/>
     <div className="homeContainer">
-      container
+      Container
     </div>
     </div>
   )
